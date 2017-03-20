a8c9b3b337a22f4d8870ac5a1d83d2fae160968a
tamami <tamami.oka@gmail.com>
2017-03-20 13:35:54 +0700

    init commit

---
+++ src/main/java/lab/aikibo/PbbInfoApplication.java
@@ -0,0 +1,12 @@
+package lab.aikibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbbInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbbInfoApplication.class, args);
+	}
+}
